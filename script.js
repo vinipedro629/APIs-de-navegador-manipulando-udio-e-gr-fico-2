f4a0f0e4a85d8252e4a9b27d550dd1f7750650fd
Pedro Vinícius <166882672+vinipedro629@users.noreply.github.com>
2025-01-27 21:11:19 -0300

    js2

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-//inicia o canvas
-var canvas = document.getElementById("detalhes");
+//iniciando o canvas
+var canvas = document.getElementById("carregamento");
 var ctx = canvas.getContext("2d");
 
-//colete o elemento do audio
-var musica = document.getElementById("musica");
+//configurações
+var x = 0;
+var y = 0;
+var altura = 10;
+var largura = 0;
+var fator = 50;
+var resolucao = 1280;
 
-//exibe a duração da música
-ctx.fillText("Duração:"+musica.duration+" segundos", 10, 10);
+//cor da barra requisitada pela equipe
+ctx.fillStyle = "#4169E1";
+
+//função que anima a barra de progresso
+function animacao(){
+    ctx.fillRect(x, y, largura = largura+fator, altura);
+    //CÓDIGO AVANÇADO: interrompe a função setInterval() para evitar carregamento excessivo
+    if(largura > resolucao){
+        clearInterval(atualiza);
+    }
+}
+
+//atualiza a barra a cada 10 milissegundos
+var atualiza = setInterval(animacao, 10);
